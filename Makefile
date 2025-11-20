b0b7bdeff46198e325739bb45a7f76827150c0c4
Julie Tardieu <jtardieu@k1r2p2.42mulhouse.fr>
2025-11-20 13:26:07 +0100

    j ai rien foutu aujourdhui

---
+++ Makefile
@@ -0,0 +1,23 @@
+NAME = get_next_line.a
+CC = cc
+CFLAGS = -Wall -Werror -Wextra
+
+SOURCE =	get_next_line.c \
+			get_next_line_utils.c
+
+OBJ = $(SOURCE:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	ar -rcs $@ $(OBJ)
+
+clean:
+	rm -rf $(OBJ)
+
+fclean: clean
+	rm -rf $(NAME)
+
+re: fclean all
+
+.PHONY : all clean fclean re
---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtardieu <jtardieu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/19 19:14:22 by jtardieu          #+#    #+#             */
+/*   Updated: 2025/11/20 13:19:08 by jtardieu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char *get_next_line(int fd);
+
+
+#endif
