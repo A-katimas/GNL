7248338c0aefdaa27e8502de1c9bb114b7773e4f
aka jtardieu <akatimasteur@gmail.com>
2025-11-27 00:08:42 +0100

    ca me saoul

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jtardieu <jtardieu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jtardieu <jtardieu@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 19:13:45 by jtardieu          #+#    #+#             */
-/*   Updated: 2025/11/26 15:39:23 by jtardieu         ###   ########.fr       */
+/*   Updated: 2025/11/26 23:43:00 by jtardieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,46 +19,44 @@ size_t	ft_strlen(const char *s);
 char *get_next_line(int fd)
 {
 	size_t		strlen = 0;
-	static char str[BUFFER_SIZE];
-	// char		*tampon ;
+	static char str[1024][BUFFER_SIZE];
+	char		*tampon ;
 	ssize_t		miss ;
 	int j = 0;
 	int i = 0;
-	// printf("\tla str(%s)\n",str);
-	while (str[i]!='\0'&& str[i]!='\n' && i!=BUFFER_SIZE)
+	tampon = "";
+	while (str[fd][i]!='\0'&& str[fd][i]!='\n' && i!=BUFFER_SIZE)
 		i++;
 	if (i == BUFFER_SIZE)
 		i = 0;
-	if (!str[i])
+	if (!str[fd][i])
 	{
-		miss = read(fd, str,BUFFER_SIZE );
+		miss = read(fd, str[fd],BUFFER_SIZE );
 		if (miss < BUFFER_SIZE)
 			return NULL;
 	}
-	i--;
-	while (str[++i]&&str[i] != '\n')
+	if (i == 0)
+		i--;
+	while (str[fd][++i]&&str[fd][i] != '\n')
 	{
-
 		if (i == BUFFER_SIZE-1)
 		{
 			i = -1;
-			miss = read(fd, str,BUFFER_SIZE );
+			tampon = ft_strjoin(tampon, str[fd]);
+			miss = read(fd, str[fd],BUFFER_SIZE );
 			if (miss < BUFFER_SIZE)
 				return NULL;
-			// printf(" |%s| ", str);
 		}
 		strlen++;
 		j++;
 
 	}
+	tampon = ft_strjoin(tampon, str[fd]);
 	printf("%d",i);
 	char *str_return = ft_calloc(sizeof(char *), j+1);
 
-	str_return = ft_strjoin(str_return, str);
+	str_return = ft_strjoin(str_return, tampon);
 
-
-	// printf("\treturn -%s-\n", str_return);
-	// printf("\tfin du masacre \n");
 	return(str_return);
 }
 
